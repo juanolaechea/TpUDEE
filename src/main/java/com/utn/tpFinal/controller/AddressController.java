b2dac1e399601a48639074496656589b5d58f592
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2021-05-10 20:30:23 -0300

    address Controller,Repository,Service

---
+++ src/main/java/com/utn/tpFinal/controller/AddressController.java
@@ -0,0 +1,34 @@
+package com.utn.tpFinal.controller;
+
+
+import com.utn.tpFinal.domain.Address;
+import com.utn.tpFinal.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequestMapping("/Address")
+public class AddressController {
+
+
+    private AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PostMapping
+    public void addAddress(@RequestBody Address newAddress){
+        addressService.addAddress(newAddress);
+    }
+    @GetMapping({"idAdress"})
+    public Address getAddressById(@PathVariable Integer IdAddress){
+
+        return addressService.getAddressById(IdAddress);
+    }
+
+
+}
---
+++ src/main/java/com/utn/tpFinal/service/AddressService.java
@@ -0,0 +1,29 @@
+package com.utn.tpFinal.service;
+
+import com.utn.tpFinal.domain.Address;
+import com.utn.tpFinal.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Service
+public class AddressService {
+
+    private AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public void addAddress(Address newAddress){
+        addressRepository.save(newAddress);
+    }
+
+
+    public Address getAddressById(Integer idAddress) {
+        return addressRepository.findById(idAddress)
+                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+    }
+}
